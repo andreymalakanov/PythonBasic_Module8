19d94795a4dce52c437eb890d9ff28889bd2df9b
andreymalakanov <101170296+andreymalakanov@users.noreply.github.com>
2022-05-28 19:20:14 +0300

    Update Task_7.py
    Task_7 done

--- Task_7.py
+++ Task_7.py
@@ -9,3 +9,15 @@ print('Задача 7. Стипендия')
 # Составьте программу расчета суммы денег,
 # которую необходимо получить у родителей один раз в начале обучения,
 # чтобы можно было прожить учебный год (10 месяцев), используя только эти деньги и стипендию.
+
+educational_grant = int(input('Введите сумму стипендии: '))
+expenses = int(input('Введите сумму расходов в первый месяц: '))
+current_mounth = 1
+for mounth in range(0, 10):
+  if educational_grant >= expenses:
+    print(f'В этом({current_mounth}) месяце стипендия покрывает все рассходы')
+  else:
+    parants_money = (int((expenses - educational_grant) * 100)) / 100
+    print(f'В этом месяце ({current_mounth}) потребуется {parants_money} рублей сверх стипендии')
+  current_mounth += 1
+  expenses *= 1.03
